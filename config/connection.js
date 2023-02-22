749a13ba32f7cb30a4a5e0de6a5f0e7024351aa0
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-02-22 17:19:11 +0300

    setup sequelize and tested its connection to the database

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
+  dialect: 'mysql'
+});
+
+
+// sequelize.authenticate().then(result => {
+//     console.log("Connection successful")
+// }).catch(err => {
+//     console.log("No connection")
+// })
