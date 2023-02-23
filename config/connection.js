90b811de17a60d7f19cc302b0566fcfd66f9725f
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-02-23 10:14:28 +0300

    added a database port

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,10 @@ dotenv.config()
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
+    port: process.env.DATABASE_PORT,
     dialect: 'mysql'
   });
-  
+
 // console.log(process.env.DATABASE)
 // console.log(process.env.USER)
 // console.log(process.env.PASSWORD)
