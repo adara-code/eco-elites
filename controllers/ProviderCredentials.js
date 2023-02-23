587b8baec0bf3931a32ab6368f9ce1ee00fa446d
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-02-23 10:30:28 +0300

    reset the controller functions

--- controllers/ProviderCredentials.js
+++ controllers/ProviderCredentials.js
@@ -14,84 +14,85 @@ dotenv.config()
 
 // signs up the provider and generates a customized token for them
 const providerSignUp = async (req, res) => {
-    // Provider.findAll({
-    //     where : {
-    //         email: req.body.providerEmail
-    //     }
-    // }).then(rs => {
-    //     if(rs.length >= 1) {
-    //         res.status(200).json([{message:"Email Exists"}])
-    //     } else {
-    //         Provider.create({
-    //             fullNames : req.body.providerNames,
-    //             email: req.body.providerEmail,
-    //             phoneNumber: req.body.providerNumber,
-    //             password: bcrypt.hashSync(req.body.providerPassword, salt)
-    //         }).then(rs => {
-    //             const providerToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-    //             res.status(200).json([{ message: providerToken }])
-    //         }).catch(err => {
-    //             res.status(200).json([{message: "Registration Failed"}])
-    //         })
+    Provider.findAll({
+        where : {
+            email: req.body.providerEmail
+        }
+    }).then(rs => {
+        if(rs.length >= 1) {
+            res.status(200).json([{message:"Email Exists"}])
+        } else {
+            Provider.create({
+                fullNames : req.body.providerNames,
+                email: req.body.providerEmail,
+                phoneNumber: req.body.providerNumber,
+                password: bcrypt.hashSync(req.body.providerPassword, salt)
+            }).then(rs => {
+                const providerToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+                res.status(200).json([{ message: providerToken }])
+            }).catch(err => {
+                res.status(200).json([{message: "Registration Failed"}])
+            })
             
-    //     }
-    // }).catch(err => {
-    //     console.log(err)
-    // })
-    res.send("It's working")
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+
+    // res.send("It's working")
 }
 
 // provider login credentials
 const providerLogin = async (req, res) => {
-    // Provider.findOne({
-    //     where: {
-    //         email : req.body.providerEmail
-    //     }
-    // }).then(rs => {
-    //     if(rs) {
-    //         // console.log(rs.dataValues.phoneNumber)
-    //         const checkPassword = bcrypt.compareSync(req.body.providerPassword,rs.dataValues.password)
-    //         if(checkPassword) {
-    //             res.status(200).json([{message:'Login Succesful'}])
-    //         }else {
-    //             res.status(200).json([{message:'username or password is invalid'}])
-    //         }
-    //     } else {
-    //         res.status(200).json([{message: 'username or password is invalid'}])
-    //     }
-    // }).catch(err => {
-    //     console.log(err)
-    // })
+    Provider.findOne({
+        where: {
+            email : req.body.providerEmail
+        }
+    }).then(rs => {
+        if(rs) {
+            // console.log(rs.dataValues.phoneNumber)
+            const checkPassword = bcrypt.compareSync(req.body.providerPassword,rs.dataValues.password)
+            if(checkPassword) {
+                res.status(200).json([{message:'Login Succesful'}])
+            }else {
+                res.status(200).json([{message:'username or password is invalid'}])
+            }
+        } else {
+            res.status(200).json([{message: 'username or password is invalid'}])
+        }
+    }).catch(err => {
+        console.log(err)
+    })
 
-    res.send("It's working")
+    // res.send("It's working")
 }
 
 // update provider password
 const resetPasswordProvider = async (req, res) => {
-    // Provider.findOne({
-    //     where : {
-    //         email : req.body.providerEmail
-    //     }
-    // }).then(rs => {
-    //     if(rs) {
-    //         Provider.update({
-    //             password: bcrypt.hashSync(req.body.resetPasswordProvide, salt)
-    //         }, {
-    //             where: {
-    //                 providerId: req.decoded.providerId
-    //             }
-    //         }
+    Provider.findOne({
+        where : {
+            email : req.body.providerEmail
+        }
+    }).then(rs => {
+        if(rs) {
+            Provider.update({
+                password: bcrypt.hashSync(req.body.resetPasswordProvide, salt)
+            }, {
+                where: {
+                    providerId: req.decoded.providerId
+                }
+            }
             
-    //         )
-    //         res.status(200).json([{message: "Password updated"}])
-    //     } else {
-    //         res.status(200).json([{message: "Email not found"}])
-    //     }
-    // }).catch(err => {
-    //     console.log(err)
-    // })
+            )
+            res.status(200).json([{message: "Password updated"}])
+        } else {
+            res.status(200).json([{message: "Email not found"}])
+        }
+    }).catch(err => {
+        console.log(err)
+    })
 
-    res.send("It's working")
+    // res.send("It's working")
 
 
     // await Provider 
@@ -100,17 +101,17 @@ const resetPasswordProvider = async (req, res) => {
 
 // create provider profile
 const providerProfile = async (req,res) => {
-    // ProviderProfile.create({
-    //     fullNames : req.body.providerNames,
-    //     email: req.body.providerEmail,
-    //     phoneNumber: req.body.providerNumber
-    // }).then(rs => {
-    //     res.status(200).json([{message: rs}])
-    // }).catch(err => {
-    //     console.log(err)
-    // })
+    ProviderProfile.create({
+        fullNames : req.body.providerNames,
+        email: req.body.providerEmail,
+        phoneNumber: req.body.providerNumber
+    }).then(rs => {
+        res.status(200).json([{message: rs}])
+    }).catch(err => {
+        console.log(err)
+    })
 
-    res.send("It's working")
+    // res.send("It's working")
 }
 
 
@@ -118,8 +119,8 @@ module.exports = {providerSignUp, providerLogin, resetPasswordProvider, provider
 
 
 
-sequelize.sync({ force: true }).then(rs => {
-    console.log(rs)
-}).catch(err => {
-    console.log(err)
-})
+// sequelize.sync({ force: true }).then(rs => {
+//     console.log(rs)
+// }).catch(err => {
+//     console.log(err)
+// })
