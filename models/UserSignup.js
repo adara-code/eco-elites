4a80521ad31b6426d2dabed44d3a1de9be3348f2
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-04 17:36:34 +0300

    user id created in the database

--- models/UserSignup.js
+++ models/UserSignup.js
@@ -3,6 +3,12 @@ const sequelize = require('../config/connection')
 
 
 const UserSignUp = sequelize.define('signup', {
+    userId : {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
     email : {
         type: Sequelize.DataTypes.STRING,
         allowNull: false,
