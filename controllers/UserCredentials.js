f4fdc112a04e217b489ddf53b653db69fa3b2345
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-06 13:24:33 +0300

    tables have been recreated on the cloud correctly

--- controllers/UserCredentials.js
+++ controllers/UserCredentials.js
@@ -11,66 +11,66 @@ const { ProfileSetup } = require('../models/ProfileSetup')
 const salt = bcrypt.genSaltSync(10)
 
 const signup = async (req,res) => {
-    // const userEmail = req.body.emailSignup
-    // const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
+    const userEmail = req.body.emailSignup
+    const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
 
-    // UserSignUp.findAll({
-    //     where: {
-    //         email : userEmail
-    //     }
-    // }).then(rs => {
-    //     if(rs.length > 0) {
-    //         res.status(200).json([{message: "Email taken. Try again"}])
-    //     } else {
-    //         UserSignUp.create({
-    //             email : userEmail,
-    //             password: userPassword
-    //         }).then(rs => {
-    //             const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-    //             res.status(200).json([{message: "Signup successful"}])
-    //             console.log(userToken)
-    //         })
-    //     }
-    // }).catch(err => {
-    //     console.log(err)
-    // })
+    UserSignUp.findAll({
+        where: {
+            email : userEmail
+        }
+    }).then(rs => {
+        if(rs.length > 0) {
+            res.status(200).json([{message: "Email taken. Try again"}])
+        } else {
+            UserSignUp.create({
+                email : userEmail,
+                password: userPassword
+            }).then(rs => {
+                const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+                res.status(200).json([{message: "Signup successful"}])
+                console.log(userToken)
+            })
+        }
+    }).catch(err => {
+        console.log(err)
+    })
 
-    res.status(200).json([{message: "Touchdown"}])
+    // res.status(200).json([{message: "Touchdown"}])
 }
 
 const newsSubscription = async(req,res) => {
-    // NewsSubscription.create({
-    //     email: req.body.emailSubscription
-    // }).then(rs => {
-    //     res.status(200).json([{message: "Subscribed"}])
-    // }).catch(err => {
-    //     res.status(200).json([{message: err}])
-    // })
-    res.status(200).json([{message: "Touchdown"}])
+    NewsSubscription.create({
+        email: req.body.emailSubscription
+    }).then(rs => {
+        res.status(200).json([{message: "Subscribed"}])
+    }).catch(err => {
+        res.status(200).json([{message: err}])
+    })
+    // res.status(200).json([{message: "Touchdown"}])
 }
 
 const profileSetup = async(req,res) => {
-    // const userProfile = {
-    //  firstName : req.body.fName,
-    //  lastName : req.body.lName,
-    //  email : req.body.uEmail,
-    //  phoneNumber : req.body.uPhoneNumber,
-    //  gender : req.body.uGender,
-    //  state : req.body.uState,
-    //  typeOfUser : req.body.uTypeOfUser,
-    //  city : req.body.uCity,
-    //  street : req.body.uStreet,
-    //  signupUserId: req.decoded.userId
-    // }
+    const userProfile = {
+     firstName : req.body.fName,
+     lastName : req.body.lName,
+     email : req.body.uEmail,
+     phoneNumber : req.body.uPhoneNumber,
+     gender : req.body.uGender,
+     state : req.body.uState,
+     typeOfUser : req.body.uTypeOfUser,
+     city : req.body.uCity,
+     street : req.body.uStreet,
+     signupUserId: req.decoded.userId
+    }
 
-    // ProfileSetup.create(userProfile).then(rs => {
-    //     res.status(200).json([{message: "Profile created"}])
-    //     console.log(rs)
-    // }).catch(err => {
-    //     console.log(err)
-    // })
+    ProfileSetup.create(userProfile).then(rs => {
+        res.status(200).json([{message: "Profile created"}])
+        console.log(rs)
+    }).catch(err => {
+        console.log(err)
+    })
 
-        res.status(200).json([{message: "Touchdown"}])
+        // res.status(200).json([{message: "Touchdown"}])
 
 }
 
