653094631c8cac962be434113ef4c6bebec29048
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-07 23:38:47 +0300

    changed origin again

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const app = express()
 
 
 // enable api to be accessible cross-origin
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // enables data to be passed through req.body
 app.use(express.json())
