b9c133381113e77e82d32100721d2c16ed38f493
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-07 23:36:33 +0300

    changed origin again

--- index.js
+++ index.js
@@ -9,16 +9,9 @@ const app = express()
 
 
 // enable api to be accessible cross-origin
-const whitelist = ['http://localhost:3000', 'https://planetprotectors.netlify.app']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  }
-}
+app.use(cors({
+    origin: '*'
+}));
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -34,7 +27,7 @@ app.use(express.urlencoded({extended: true}))
 const PORT = process.env.PORT
 
 // route setup
-app.use('/', cors(corsOptions),routing)
+app.use('/', routing)
 
 
 // run server
