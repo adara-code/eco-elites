158e3cea375498bb53bf266cdc6fc78266a5f704
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-08 22:15:55 +0300

    cors testing v4

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const routing = require('./routes/rts')
 dotenv.config()
 const app = express()
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods:["GET","POST"]
-  })
-)
+app.use(function(req,res,next){
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+})
 // enables data to be passed through req.body
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
