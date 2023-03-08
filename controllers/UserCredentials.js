60c47ed9e67c2f87ae1e7004ec0a26ce8af7afba
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-08 21:30:12 +0300

    testing cors

--- controllers/UserCredentials.js
+++ controllers/UserCredentials.js
@@ -10,31 +10,31 @@ const { ProfileSetup } = require('../models/ProfileSetup')
 const salt = bcrypt.genSaltSync(10)
 
 const signup = async (req,res) => {
-    const userEmail = req.body.emailSignup
-    const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
+    // const userEmail = req.body.emailSignup
+    // const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
 
-    UserSignUp.findAll({
-        where: {
-            email : userEmail
-        }
-    }).then(rs => {
-        if(rs.length > 0) {
-            res.status(200).json([{message: "Email taken. Try again"}])
-        } else {
-            UserSignUp.create({
-                email : userEmail,
-                password: userPassword
-            }).then(rs => {
-                const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-                res.status(200).json([{message: "Signup successful"}])
-                console.log(userToken)
-            })
-        }
-    }).catch(err => {
-        console.log(err)
-    })
+    // UserSignUp.findAll({
+    //     where: {
+    //         email : userEmail
+    //     }
+    // }).then(rs => {
+    //     if(rs.length > 0) {
+    //         res.status(200).json([{message: "Email taken. Try again"}])
+    //     } else {
+    //         UserSignUp.create({
+    //             email : userEmail,
+    //             password: userPassword
+    //         }).then(rs => {
+    //             const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+    //             res.status(200).json([{message: "Signup successful"}])
+    //             console.log(userToken)
+    //         })
+    //     }
+    // }).catch(err => {
+    //     console.log(err)
+    // })
     // res.status(200).json([{message: "Signup successful"}])
-    // res.status(200).json([{message: "Touchdown"}])
+    res.status(200).json([{message: "Touchdown"}])
 }
 
 const newsSubscription = async(req,res) => {
