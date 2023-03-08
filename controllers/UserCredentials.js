e9d888a367092e47ff80f364278685e3ea4c7590
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-08 20:21:13 +0300

    testing the database

--- controllers/UserCredentials.js
+++ controllers/UserCredentials.js
@@ -10,30 +10,30 @@ const { ProfileSetup } = require('../models/ProfileSetup')
 const salt = bcrypt.genSaltSync(10)
 
 const signup = async (req,res) => {
-    // const userEmail = req.body.emailSignup
-    // const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
+    const userEmail = req.body.emailSignup
+    const userPassword = bcrypt.hashSync(req.body.passwordSignup,salt)
 
-    // UserSignUp.findAll({
-    //     where: {
-    //         email : userEmail
-    //     }
-    // }).then(rs => {
-    //     if(rs.length > 0) {
-    //         res.status(200).json([{message: "Email taken. Try again"}])
-    //     } else {
-    //         UserSignUp.create({
-    //             email : userEmail,
-    //             password: userPassword
-    //         }).then(rs => {
-    //             const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
-    //             res.status(200).json([{message: "Signup successful"}])
-    //             console.log(userToken)
-    //         })
-    //     }
-    // }).catch(err => {
-    //     console.log(err)
-    // })
-    res.status(200).json([{message: "Touchdown"}])
+    UserSignUp.findAll({
+        where: {
+            email : userEmail
+        }
+    }).then(rs => {
+        if(rs.length > 0) {
+            res.status(200).json([{message: "Email taken. Try again"}])
+        } else {
+            UserSignUp.create({
+                email : userEmail,
+                password: userPassword
+            }).then(rs => {
+                const userToken = jwt.sign(rs.dataValues,process.env.JWT_KEY)
+                res.status(200).json([{message: "Signup successful"}])
+                console.log(userToken)
+            })
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+    // res.status(200).json([{message: "Touchdown"}])
 }
 
 const newsSubscription = async(req,res) => {
