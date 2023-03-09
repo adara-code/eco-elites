d37091b9df52d7f7e2fe4b7ef06c3d351a6ffb3b
Shee Kariuki <kariuki.w.agnes@gmail.com>
2023-03-09 14:43:54 +0300

    changed the env path

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv')
 
 
-// dotenv.config({path: '../.env'})
-dotenv.config()
+dotenv.config({path: '../.env'})
+// dotenv.config()
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.DATABASE_USER, process.env.PASSWORD, {
     host: process.env.HOST,
     port: process.env.DATABASE_PORT,
     dialect: 'mysql'
